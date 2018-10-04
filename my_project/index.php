11e8e2b53dd175d50268eae745c1004a884a2e57
Nick-Alm <alm.nicholas.2013@gmail.com>
2018-10-04 12:13:12 -0600

    16.2 - Create app page

---
+++ my_project/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    // Start the page
+    require_once 'views.php';
+
+    $site_title = 'MVC Project';
+    $page_title = 'MVC Project - Nick Alm';
+    begin_page($site_title, $page_title);
+
+
+    // Your page content goes here
+
+    // Include other content
+    require 'pattern.html';
+
+
+    // End the page
+    end_page();
+
+?>
